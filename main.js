70aba3458a3da3a8950a8c69ae836b233a897b8a
MGDeveloper-tech <mistery.of.mail1@gmail.com>
2025-02-08 15:05:50 +0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,33 @@
+const user1 = "Giorgi";
+const age1 = 20;
+
+const user2 = "Mariam"
+const age2 = 18;
+
+const color1 = "violet";
+const color2 = "blue";
+const color3 = "royal blue";
+
+const nam1 = 16;
+const nam2 = -16;
+const nam3 = 2 * 8;
+const nam4 = 2 + 8;
+const nam5 = 2 - 8;
+const nam6 = 8 / 2;
+
+const nam7 = 123 * 654;
+
+console.log(nam7);
+
+console.log(nam1);
+
+console.log(nam2);
+
+console.log(nam3);
+
+console.log(nam4);
+
+console.log(nam5);
+
+console.log(nam6);
+
